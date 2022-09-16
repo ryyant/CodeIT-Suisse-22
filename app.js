3424bf0111a750bf0b54e207c6d38d7854e75c14
ryyant <ryyant@hotmail.com>
2022-09-16 15:09:05 +0800

    Fix output format

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const morganBody = require("morgan-body");
 const PORT = process.env.PORT || 3000;
-const { to_cumulative_delayed, to_cumulative } = require("./Tasks/TickerStream");
+const {
+  to_cumulative_delayed,
+  to_cumulative,
+} = require("./Tasks/TickerStream");
 
 const app = express().use(express.json());
 morganBody(app, { noColors: process.env.NODE_ENV === "production" });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.json("Server connected!");
+});
 
 app.post("/square", (req, res) => {
   const output = parseInt(req.body.input) ** 2;
@@ -16,12 +23,12 @@ app.post("/square", (req, res) => {
 app.post("/tickerStreamPart1", (req, res) => {
   let stream = req.body.stream;
   const output = to_cumulative(stream);
-  res.json(output);
+  res.json({output: output});
 });
 
 app.post("/tickerStreamPart2", (req, res) => {
   let stream = req.body.stream;
   let quantityBlock = req.body.quantityBlock;
   const output = to_cumulative_delayed(stream, quantityBlock);
-  res.json(output);
+  res.json({output: output});
 });
