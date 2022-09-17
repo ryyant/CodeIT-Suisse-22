bd790b3cdf63fd65a0f8322325a320979ffb00d2
ryyant <ryyant@hotmail.com>
2022-09-17 10:16:52 +0800

    CryptoCollapz

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const {
   to_cumulative_delayed,
   to_cumulative,
 } = require("./Tasks/TickerStream");
+const { CryptoCollapz } = require("./Tasks/CryptoCollapz");
 
 const app = express().use(express.json());
 morganBody(app, { noColors: process.env.NODE_ENV === "production" });
@@ -23,12 +24,19 @@ app.post("/square", (req, res) => {
 app.post("/tickerStreamPart1", (req, res) => {
   let stream = req.body.stream;
   const output = to_cumulative(stream);
-  res.json({output: output});
+  res.json({ output: output });
 });
 
 app.post("/tickerStreamPart2", (req, res) => {
   let stream = req.body.stream;
   let quantityBlock = req.body.quantityBlock;
   const output = to_cumulative_delayed(stream, quantityBlock);
-  res.json({output: output});
+  res.json({ output: output });
+});
+
+app.post("/cryptocollapz", (req, res) => {
+  let arr = req.body;
+  console.log(arr);
+  const output = CryptoCollapz(arr);
+  res.json(output);
 });
